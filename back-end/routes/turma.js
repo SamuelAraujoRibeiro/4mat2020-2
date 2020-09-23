24fd0cd1fec34e2af467f735aa1fa7be5a4129d1
SamuelAraujoRibeiro <samuelaraujo27@hotmail.com>
2020-09-23 14:12:07 +0000

    (23/09) CRUD com relacionamentos

---
+++ back-end/routes/turma.js
@@ -0,0 +1,12 @@
+const controller = require('../controllers/turma')
+const express = require('express')
+
+const router = express.Router()
+
+router.post('/', controller.novo) // Create
+router.get('/', controller.listar) // Retrieve(all)
+router.get('/:id', controller.obterUm) // Retrive(one)
+router.put('/', controller.atualizar) // Update
+router.delete('/', controller.excluir) // Delete
+
+module.exports = router
