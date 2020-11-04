3d78e92ce4e1da6ec7fa1f7b1a2cefb2d637ca62
SamuelAraujoRibeiro <samuelaraujo27@hotmail.com>
2020-11-04 14:07:34 +0000

    (04/11) Primeiro service e primeiro componente de listagem

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiServer: 'https://3000-bf2ef64a-fde3-45f0-983d-d58bd553054b.ws-us02.gitpod.io/'
 };
 
 /*
