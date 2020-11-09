9b32267aa29d5c0e5f4b893e47e871bc48db9ddd
SamuelAraujoRibeiro <samuelaraujo27@hotmail.com>
2020-11-09 14:11:53 +0000

    (09/11) Criação da lista de cursos

--- back-end/app.js
+++ back-end/app.js
@@ -14,6 +14,9 @@ db(`mongodb+srv://${dbUser}:${dbPass}@cluster0.8mu4y.gcp.mongodb.net/${dbName}?r
 
 var app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
